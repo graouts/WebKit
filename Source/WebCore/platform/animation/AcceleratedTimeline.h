c8ab05027e894d0171a85da9614f8fb598d52342
Antoine Quint <graouts@webkit.org>
2024-10-24 12:22:23 +0200

    First stab at adding accelerated timelines.

---
+++ Source/WebCore/platform/animation/AcceleratedTimeline.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2024 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#if ENABLE(THREADED_ANIMATION_RESOLUTION)
+
+#include "WebAnimationTime.h"
+#include <wtf/Ref.h>
+#include <wtf/TZoneMalloc.h>
+#include <wtf/WeakPtr.h>
+
+namespace WebCore {
+
+class AcceleratedTimeline : public RefCounted<AcceleratedTimeline>, public CanMakeWeakPtr<AcceleratedTimeline> {
+    WTF_MAKE_TZONE_OR_ISO_ALLOCATED(AcceleratedTimeline);
+public:
+    virtual ~AcceleratedTimeline() = default;
+
+    WEBCORE_EXPORT virtual bool isDocumentTimeline() const { return false; }
+    WEBCORE_EXPORT virtual bool isScrollTimeline() const { return false; }
+    WEBCORE_EXPORT virtual bool isViewTimeline() const { return false; }
+
+    WEBCORE_EXPORT virtual std::optional<WebAnimationTime> currentTime() { return std::nullopt; }
+
+protected:
+    AcceleratedTimeline(std::optional<WebAnimationTime> duration);
+
+private:
+    std::optional<WebAnimationTime> m_duration;
+};
+
+} // namespace WebCore
+
+#define SPECIALIZE_TYPE_TRAITS_ACCELERATED_TIMELINE(ToValueTypeName, predicate) \
+SPECIALIZE_TYPE_TRAITS_BEGIN(WebCore::ToValueTypeName) \
+static bool isType(const WebCore::AcceleratedTimeline& value) { return value.predicate; } \
+SPECIALIZE_TYPE_TRAITS_END()
+
+#endif // ENABLE(THREADED_ANIMATION_RESOLUTION)
